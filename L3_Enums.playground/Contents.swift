5b5409c79b6cce07b978ece50dceba9cccecab37
christopheradolphe <christopher@a2xaccounting.com>
2024-03-20 15:52:28 -0400

    Lecture 3 - Enumerations

---
+++ L3_Enums.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Cocoa
+
+//Purpose of Enums
+// Defining a type that can only have a certain number of possible values (eg. month, weekday)
+
+//Creating Enum
+enum Weekday{
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+}
+
+//Less lines for creating Enum
+enum Months{
+    case January, February, March, April
+}
+
+
+//Using a Enum
+var day = Weekday.Monday
+print(day)
+
+var currentBill = Months.April
+print(currentBill)
+
+//Seem to be unable to do enums with integers
