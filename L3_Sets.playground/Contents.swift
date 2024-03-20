e9e967d75b49a2f4ed962ea2e21cdf5d734226a5
christopheradolphe <christopher@a2xaccounting.com>
2024-03-20 15:39:09 -0400

    Lesson 3 - Sets

---
+++ L3_Sets.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+//Sets are made from Arrays
+let actor = Set(["Denzel", "Tom Cruise", "Nicholas Cage", "The Rock"])
+
+//Important Info:
+//1. Set will automatically remove duplicates
+//2. Will not remember order in origianl array (eg. print order is random)
+
+print(actor)
+
+//Creating a Set
+var actors = Set<Strings>()
+
+//Adding to set -> .insert()
+actors.insert("Chris")
+actors.insert("James")
+actors.insert("Jimmy")
+
+//Number of Elements in Set -> .count
+print(actors.count)
+
+//Sorting a Set -> .sorted()
