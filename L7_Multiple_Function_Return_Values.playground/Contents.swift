414d79497382a8509e88aca5f2b7d5d0a2b23514
christopheradolphe <ckwadolphe@gmail.com>
2024-03-24 02:06:38 -0400

    Lecture 7 - Having multiple return values for function

---
+++ L7_Multiple_Function_Return_Values.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+//Could return string and dictionary return as one value but messy
+//Overall use one of Sets, Arrays, Dictionaries or Tuples to return
+//Tuples are cleanest
+
+func getUser() -> (firstName: String, lastName: String) {
+    return ("Taylor", "Swift")
+}
+
+print("Our user is \(getUser().firstName) \(getUser().lastName)")
+
+//Pulling values from the tuple
+let (firstName, lastName) = getUser()
