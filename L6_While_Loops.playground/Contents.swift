320cb737f3630109950aff7e85d6ce61c389b196
christopheradolphe <ckwadolphe@gmail.com>
2024-03-24 00:43:14 -0400

    Lecture 6 - While loop notes

--- L6_While_Loops.playground/Contents.swift
+++ L6_While_Loops.playground/Contents.swift
@@ -1,3 +1,22 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+//When to use instead of for loop
+// Until arbitrary condition becomes false
+
+//Checks case when loop completes and not during
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off")
+
+
+var roll = 0
+
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled \(roll)")
+}
