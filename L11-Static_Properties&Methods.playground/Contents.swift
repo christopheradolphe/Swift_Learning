e5ea70e3587b5f5a0bd321c0f5fff9ec81e5af90
christopheradolphe <ckwadolphe@gmail.com>
2024-04-03 01:36:23 -0400

    Start of static properties and function notes

---
+++ L11-Static_Properties&Methods.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+//Main purpose:
+//1. Sroting fixed data to be access in multiple instances
+//2. Creating example data
+
+//Essentially all instances of struct will all have same function or 
