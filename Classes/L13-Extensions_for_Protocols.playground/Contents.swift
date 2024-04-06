7947a8e3471ace1e821ab754221105acd57da92f
christopheradolphe <ckwadolphe@gmail.com>
2024-04-06 02:47:45 -0400

    Protocol Extensions

---
+++ Classes/L13-Extensions_for_Protocols.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+//Protocol Extensions = Extensions written for protocols
+//Can extend protocols to include method implmentations
+
+//Usefulness of Protocol Extensions
+//When we want to copy functionality to structs/classes that conform to particular protocol
+
+extension Array {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+if guests.isNotEmpty {
+    print("There are some guests")
+}
+
+
+//**Note: Arrays, Sets and Dictionaries all conform to protocol Collection
+
+//Protocol Extensions
+//So if we wanted all Collection to contain isNotEmpty we could extend protocol
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+//Default Protocol methods or properties
+//We can add default implementations of the method as well (also shown above) but can only do inside extensions not inside protocol
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
