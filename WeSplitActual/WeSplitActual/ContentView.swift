fd3563da96609ce8fb29a6ed6a5ce3848901f51a
christopheradolphe <ckwadolphe@gmail.com>
2024-04-07 19:09:10 -0400

    Create WeSplit Project

---
+++ WeSplitActual/WeSplitActual/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplitActual
+//
+//  Created by Christopher Adolphe on 2024-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplitActual/WeSplitActual/WeSplitActualApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitActualApp.swift
+//  WeSplitActual
+//
+//  Created by Christopher Adolphe on 2024-04-07.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitActualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
