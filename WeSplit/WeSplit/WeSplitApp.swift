7ef9ac5745c27b959f78a156746bffd2bdaa4eaf
christopheradolphe <ckwadolphe@gmail.com>
2024-04-07 16:52:28 -0400

    Create we split app

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Christopher Adolphe on 2024-04-07.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
