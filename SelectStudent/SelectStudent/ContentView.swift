95d47f679f0ed2138ceebad6382cef41933467e2
christopheradolphe <ckwadolphe@gmail.com>
2024-04-07 19:05:18 -0400

    Finish select a student

--- SelectStudent/SelectStudent/ContentView.swift
+++ SelectStudent/SelectStudent/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermoince", "Ron"]
+    @State private var selectedStudent = "Harry"
     var body: some View {
-        let students = ["Harry", "Hermoince", "Ron"]
-        @State private var selectedStudent = "Harry"
         NavigationStack{
             Form {
                 Picker ("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
+                    ForEach(students, id: \.self) { //\.self tells Swift string itself is unique id
                         Text($0)
                     }
                 }
             }
+            .navigationTitle("Select a Student")
         }
     }
 }
