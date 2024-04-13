522b866380983ae5516f0906f263544146af689c
christopheradolphe <ckwadolphe@gmail.com>
2024-04-12 23:45:24 -0400

    Boiler plate for output temperature value

--- Conversions/Conversions/ContentView.swift
+++ Conversions/Conversions/ContentView.swift
@@ -12,6 +12,29 @@ struct ContentView: View {
     @State private var outputType: String = "Farhenheit"
     @State private var inputValue: Double = 0.0
     
+    var outputTempValue: Double {
+        if inputType == outputType {
+            return inputValue
+        } else if inputType == "Celsius" {
+            if outputType == "Farhenheit" {
+                return inputValue
+            } else { //Kelvin
+                return inputValue + Double(273)
+            }
+        } else if inputType == "Farhenheit" {
+            if outputType == "Celsius" {
+                return inputValue
+            } else { //Kelvin
+                return inputValue
+            }
+        } else { //Kelvin
+            if outputType == "Celsius" {
+                return inputValue
+            } else { //Farhenheit
+                return inputValue
+            }
+        }
+    }
     
     let temperatures = ["Celsius", "Farhenheit", "Kelvin"]
     var body: some View {
