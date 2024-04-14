289d86d72922b7e894671fae26914829547c59eb
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 14:39:43 -0400

    Glassy effect

--- ColoursAndFrames/ColoursAndFrames/ContentView.swift
+++ ColoursAndFrames/ColoursAndFrames/ContentView.swift
@@ -9,21 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("First Content")
-                .background(.blue) //For one text piece
-            Text("Your Content")
-        }
-        .background(.yellow) //For entire text content in stack
+//        VStack {
+//            Text("First Content")
+//                .background(.blue) //For one text piece
+//            Text("Your Content")
+//        }
+//        .background(.yellow) //For entire text content in stack
+//        
+//        ZStack {
+////            Color(red:2,blue:4) //Combine to make new colour
+//            Color.green //Make whole stack one colour
+////                .frame(width: 500, height: 200) //Specify size of colour
+//                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+//            Text("All one colour")
+//        }
+//        .ignoresSafeArea() //Move into corners of screen
         
         ZStack {
-//            Color(red:2,blue:4) //Combine to make new colour
-            Color.green //Make whole stack one colour
-//                .frame(width: 500, height: 200) //Specify size of colour
-                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-            Text("All one colour")
+            VStack(spacing: 0) {
+                Color.red
+                Color.green
+            }
+            //Create a glassy effect on a background
+            Text("Your Content")
+                .foregroundStyle(.secondary) //Opacity
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
-        .ignoresSafeArea() //Move into corners of screen
     }
 }
 
