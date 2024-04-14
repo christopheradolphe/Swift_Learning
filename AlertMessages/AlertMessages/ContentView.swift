46bfacefe285b57b2e8c0b3c7744a873b46c0fd1
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 15:27:16 -0400

    Alert messages

--- AlertMessages/AlertMessages/ContentView.swift
+++ AlertMessages/AlertMessages/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Show Alert") {
+                showingAlert = true
+            }
         }
         .padding()
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Please read this")
+        }
     }
 }
 
