60f0c5db97e2143556369131859ff5d131d6ce8b
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 14:08:06 -0400

    Guess the flag project start

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Christopher Adolphe on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
