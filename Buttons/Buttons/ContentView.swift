4249c44c9762f27f81283048f6a0a3cd155efcc5
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 15:00:58 -0400

    Button notes

--- Buttons/Buttons/ContentView.swift
+++ Buttons/Buttons/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Button("Delete Selection", action: executeDelete)
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button ("Button 1") { }
+                .buttonStyle(.bordered)
+            Button ("Button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            
+            //Custom button
+            Button {
+                executeDelete()
+            } label: {
+                Text("Tap Me")
+                    .padding()
+                    .foregroundStyle(.blue)
+                    .background(.green)
+            }
         }
-        .padding()
+    }
+    func executeDelete() {
+        print("Now deleting ..")
     }
 }
 
