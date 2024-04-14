bc1259b6f15f9c1fc54243948f4041d668182b4d
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 14:21:06 -0400

    Spacer notes

--- StacksToArrangeViews/StacksToArrangeViews/ContentView.swift
+++ StacksToArrangeViews/StacksToArrangeViews/ContentView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) { //Alignment/Spacing
+            Spacer()
             Text("Hello1")
+            Spacer() //Takes up max space between
             Text("Hello2")
+            Spacer()
         }
-        HStack(spacing: 20) { //Spacing for Hstack
-            Text("Hello1")
-            Text("Hello2")
-        }
-        ZStack { //Stacked on top of each other
-            Text("Hello 1")
-            Text("Hello2")
-        }
+        
+        
+        
+        
+        
+        
+//        HStack(spacing: 20) { //Spacing for Hstack
+//            Text("Hello1")
+//            Text("Hello2")
+//        }
+//        ZStack { //Stacked on top of each other
+//            Text("Hello 1")
+//            Text("Hello2")
+//        }
     }
 }
 
