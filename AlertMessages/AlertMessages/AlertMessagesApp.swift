630ef818ba9799fa2b5aacbbb20e03a2ab9a3dee
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 15:19:11 -0400

    Alert Messages note start

---
+++ AlertMessages/AlertMessages/AlertMessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertMessagesApp.swift
+//  AlertMessages
+//
+//  Created by Christopher Adolphe on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct AlertMessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
