ae119693aef1b3b4b98020384512e52a434d7900
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 14:35:44 -0400

    Move colours into safe area, combining colours

--- ColoursAndFrames/ColoursAndFrames/ContentView.swift
+++ ColoursAndFrames/ColoursAndFrames/ContentView.swift
@@ -17,11 +17,13 @@ struct ContentView: View {
         .background(.yellow) //For entire text content in stack
         
         ZStack {
+//            Color(red:2,blue:4) //Combine to make new colour
             Color.green //Make whole stack one colour
 //                .frame(width: 500, height: 200) //Specify size of colour
                 .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
             Text("All one colour")
         }
+        .ignoresSafeArea() //Move into corners of screen
     }
 }
 
