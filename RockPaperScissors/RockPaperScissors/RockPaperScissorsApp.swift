aeb097852364774108f4471d583e0de58b00931b
christopheradolphe <ckwadolphe@gmail.com>
2024-04-14 23:41:34 -0400

    Rock Paper Scissors Start of project

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Christopher Adolphe on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
