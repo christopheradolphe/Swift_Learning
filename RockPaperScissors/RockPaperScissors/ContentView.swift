1181d06900b787de5edceea6e8ed9a79f5117c86
christopheradolphe <ckwadolphe@gmail.com>
2024-04-15 00:33:00 -0400

    Finish Logic for rock paper scissors

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     @State private var Win = true
     @State private var AppMove = Int.random(in: 0...2)
     @State private var playerScore = 0
-    private var numberQuestions = 0
+    @State private var numberQuestions = 0
+    @State private var gameOver = false
     
     let elements = ["Rock", "Paper", "Scissors"]
     var body: some View {
@@ -47,11 +48,41 @@ struct ContentView: View {
             }
             .navigationTitle("Rock - Paper - Scissors")
         }
+        
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("Restart Game", action: restartGame)
+        } message: {
+            Text("Final Game Score: \(playerScore)/\(numberQuestions)")
+        }
+
     }
     func checkSelection(_ number: Int) -> Void {
         if Win {
-            if (number + 2) 
+            if (number + 2) % 3 == AppMove {
+                playerScore += 1
+            }
+        } else {
+            if (number + 1) % 3 == AppMove {
+                playerScore += 1
+            }
         }
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        Win.toggle()
+        numberQuestions += 1
+        if numberQuestions > 9 {
+            gameOver = true
+        }
+        AppMove = Int.random(in: 0...2)
+    }
+    
+    func restartGame() {
+        Win = true
+        numberQuestions = 0
+        AppMove = Int.random(in: 0...2)
+        playerScore = 0
     }
 }
 
