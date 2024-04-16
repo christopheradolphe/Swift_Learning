3a5c825de9a566ccb285a693ce6bfc6011bd4190
christopheradolphe <ckwadolphe@gmail.com>
2024-04-16 00:23:39 -0400

    Create an ML model with CreateML

---
+++ CoreML/CoreML/CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLApp.swift
+//  CoreML
+//
+//  Created by Christopher Adolphe on 2024-04-16.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
