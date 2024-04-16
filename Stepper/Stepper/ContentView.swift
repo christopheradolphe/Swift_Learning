9aaf0a778ca58674b542a98a86c59e3cd788e301
christopheradolphe <ckwadolphe@gmail.com>
2024-04-15 23:26:43 -0400

    Stepper Notes

---
+++ Stepper/Stepper/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Stepper
+//
+//  Created by Christopher Adolphe on 2024-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepTime = 8.0
+    var body: some View {
+        Stepper("\(sleepTime) hours", value: $sleepTime, in: 4...12, step: 0.25)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stepper/Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Christopher Adolphe on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
