b5b3989e2689173d94ec280adf2c04ff22d50ed7
christopheradolphe <ckwadolphe@gmail.com>
2024-04-16 16:22:54 -0400

    Integrate basic UI for BetterRest app

---
+++ BetterRestApp/BetterRestApp/BetterRestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestAppApp.swift
+//  BetterRestApp
+//
+//  Created by Christopher Adolphe on 2024-04-16.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
