fafcf4b991750e28f65911cddce3584ce8265408
christopheradolphe <ckwadolphe@gmail.com>
2024-04-20 00:04:39 -0400

    Start of Word Scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Christopher Adolphe on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
