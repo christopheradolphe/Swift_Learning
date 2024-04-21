d1510de98806f3f0ecb54d1c80112cdf953810cb
christopheradolphe <ckwadolphe@gmail.com>
2024-04-21 18:34:34 -0400

    Cool way to highlight a button to click

---
+++ EncourageButtonClickAnimation/EncourageButtonClickAnimation/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  EncourageButtonClickAnimation
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button ("Submit") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.green)
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        //Suggest button click
+        .overlay(
+            Circle()
+                .stroke(.green)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EncourageButtonClickAnimation/EncourageButtonClickAnimation/EncourageButtonClickAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncourageButtonClickAnimationApp.swift
+//  EncourageButtonClickAnimation
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct EncourageButtonClickAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
