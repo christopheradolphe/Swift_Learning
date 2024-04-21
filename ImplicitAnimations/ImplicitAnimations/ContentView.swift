5cab2b44ed62090e3931a30d0bcf4baa5a8c65c5
christopheradolphe <ckwadolphe@gmail.com>
2024-04-21 18:06:37 -0400

    Implicit Animations note

---
+++ ImplicitAnimations/ImplicitAnimations/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  ImplicitAnimations
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        //To animate scaleEffect and blur
+        .animation(.default, value: animationAmount)
+    }
+}
+
+#Preview {
+    ContentView()
+}
