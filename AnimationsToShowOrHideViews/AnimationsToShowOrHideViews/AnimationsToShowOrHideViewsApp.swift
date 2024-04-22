7667b5fa50efef72e9b1b4b9ae3999bcec79da12
christopheradolphe <ckwadolphe@gmail.com>
2024-04-22 00:03:14 -0400

    Animations to show or hide views

---
+++ AnimationsToShowOrHideViews/AnimationsToShowOrHideViews/AnimationsToShowOrHideViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsToShowOrHideViewsApp.swift
+//  AnimationsToShowOrHideViews
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsToShowOrHideViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationsToShowOrHideViews/AnimationsToShowOrHideViews/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  AnimationsToShowOrHideViews
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.scale)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
