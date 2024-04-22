19cffb3ead50695df90400673a80e3b353d53033
christopheradolphe <ckwadolphe@gmail.com>
2024-04-21 23:50:25 -0400

    Animating Gestures

---
+++ AnimatingGestures/AnimatingGestures/AnimatingGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingGesturesApp.swift
+//  AnimatingGestures
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatingGestures/AnimatingGestures/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  AnimatingGestures
+//
+//  Created by Christopher Adolphe on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in 
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            .animation(.bouncy, value: dragAmount)
+    }
+}
+
+#Preview {
+    ContentView()
+}
