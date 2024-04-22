c29f23a312ae66cfb2f5dda4b4bc15e109cd21ff
christopheradolphe <ckwadolphe@gmail.com>
2024-04-22 00:12:07 -0400

    Custom transitions

---
+++ CustomTransitions/CustomTransitions/CustomTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionsApp.swift
+//  CustomTransitions
+//
+//  Created by Christopher Adolphe on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
