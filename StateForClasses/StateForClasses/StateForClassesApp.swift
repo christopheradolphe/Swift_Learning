17c806cf14a4e064a41a33aa0beaa428431ea56f
christopheradolphe <ckwadolphe@gmail.com>
2024-04-27 17:00:59 -0400

    Start of notes for 7th project

---
+++ StateForClasses/StateForClasses/StateForClassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateForClassesApp.swift
+//  StateForClasses
+//
+//  Created by Christopher Adolphe on 2024-04-27.
+//
+
+import SwiftUI
+
+@main
+struct StateForClassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
