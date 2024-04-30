9eb3ffd002ac866314bf96bc1f3a6b11c90e3c03
christopheradolphe <ckwadolphe@gmail.com>
2024-04-30 00:28:33 -0400

    Start of iExpense app

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Christopher Adolphe on 2024-04-30.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Christopher Adolphe on 2024-04-30.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
