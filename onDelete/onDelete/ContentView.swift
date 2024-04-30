fc224b765690d2a4f729916d1a7398821fa676ec
christopheradolphe <ckwadolphe@gmail.com>
2024-04-29 23:42:36 -0400

    Delete and Edit buttons

---
+++ onDelete/onDelete/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  onDelete
+//
+//  Created by Christopher Adolphe on 2024-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ onDelete/onDelete/onDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  onDeleteApp.swift
+//  onDelete
+//
+//  Created by Christopher Adolphe on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct onDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
