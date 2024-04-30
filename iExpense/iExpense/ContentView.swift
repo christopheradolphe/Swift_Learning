c2153f6cd274a4772826991ddf8d2972fd2a219a
christopheradolphe <ckwadolphe@gmail.com>
2024-04-30 01:07:04 -0400

    Use UserDefaults to store and save user data

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,16 +7,31 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let name: String
     let type: String
     let amount: Double
-    let id = UUID()
+    var id = UUID()
 }
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey:"Items")
+            }
+        }
+    }
+    
+    init () {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+    }
 }
 
 struct ContentView: View {
