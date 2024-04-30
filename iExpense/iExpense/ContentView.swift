679c2e5d3917ca2296ac99df2646f592dd3f46b3
christopheradolphe <ckwadolphe@gmail.com>
2024-04-30 00:41:48 -0400

    UUIDand Identifiable notes

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-struct ExpenseItem {
+struct ExpenseItem: Identifiable {
     let name: String
     let type: String
     let amount: Double
+    let id = UUID()
 }
 
 @Observable
@@ -24,7 +25,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items) { item in 
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
