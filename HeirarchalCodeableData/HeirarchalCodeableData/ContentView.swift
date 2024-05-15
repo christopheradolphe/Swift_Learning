507e862dcb8495450a780a9393200b6ecc320fc2
christopheradolphe <ckwadolphe@gmail.com>
2024-05-14 23:29:44 -0400

    Heirarchal Codable Data

---
+++ HeirarchalCodeableData/HeirarchalCodeableData/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  HeirarchalCodeableData
+//
+//  Created by Christopher Adolphe on 2024-05-14.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                ""address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from:data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeirarchalCodeableData/HeirarchalCodeableData/HeirarchalCodeableDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeirarchalCodeableDataApp.swift
+//  HeirarchalCodeableData
+//
+//  Created by Christopher Adolphe on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct HeirarchalCodeableDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
