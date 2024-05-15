9f3a079935e9bb7d64d6de6d08df5222579aed88
christopheradolphe <ckwadolphe@gmail.com>
2024-05-15 00:12:20 -0400

    Create a mission struct to handle JSON of all missions

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Christopher Adolphe on 2024-05-15.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String? //May or may not be launch data
+    let crew: [CrewRole]
+    let description: String
+}
