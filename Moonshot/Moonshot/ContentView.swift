eb9288fa33d499e2f94f25aaa65ee89c929d218c
christopheradolphe <ckwadolphe@gmail.com>
2024-05-18 16:00:42 -0400

    Add grid layout for missions

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -11,8 +11,32 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text(String(astronauts.count))
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 300, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
