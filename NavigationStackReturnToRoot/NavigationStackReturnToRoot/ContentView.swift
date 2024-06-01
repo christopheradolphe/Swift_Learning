aeb759b017ed73d9e357aaa1894efb5e07d283af
christopheradolphe <ckwadolphe@gmail.com>
2024-06-01 18:16:19 -0400

    Use @Binding to pass @State to another view to get back to home view

--- NavigationStackReturnToRoot/NavigationStackReturnToRoot/ContentView.swift
+++ NavigationStackReturnToRoot/NavigationStackReturnToRoot/ContentView.swift
@@ -9,22 +9,28 @@ import SwiftUI
 
 struct DetailView: View {
     var number: Int
+    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
     }
 }
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    @State private var path = NavigationPath()
     
     var body: some View {
         //Can go infinitely deep
         NavigationStack(path: $path) {
-            DetailView(number: 0)
+            DetailView(number: 0, path: $path)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i)
+                    DetailView(number: i, path: $path)
                 }
         }
     }
