0be60efd108918da18eb477235950873c0a98cdb
christopheradolphe <ckwadolphe@gmail.com>
2024-06-01 17:33:38 -0400

    Notes on hashable and navigation destination

---
+++ NavigationDestination/NavigationDestination/NavigationDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDestinationApp.swift
+//  NavigationDestination
+//
+//  Created by Christopher Adolphe on 2024-06-01.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
