d2d2592b599e09d5c3fecf59ca93bb58d4db16e2
christopheradolphe <ckwadolphe@gmail.com>
2024-06-01 18:46:25 -0400

    Saving navigation bar appearance

---
+++ SaveNavigationStackAfterAppClose/SaveNavigationStackAfterAppClose/SaveNavigationStackAfterAppCloseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveNavigationStackAfterAppCloseApp.swift
+//  SaveNavigationStackAfterAppClose
+//
+//  Created by Christopher Adolphe on 2024-06-01.
+//
+
+import SwiftUI
+
+@main
+struct SaveNavigationStackAfterAppCloseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
