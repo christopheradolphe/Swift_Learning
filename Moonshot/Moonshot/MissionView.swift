bd7eb99e41e43a64b875f5b110f9baa347aa30f1
christopheradolphe <ckwadolphe@gmail.com>
2024-06-01 17:13:42 -0400

    Convert separator to a custom view

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+struct Separator: View {
+    var body: some View {
+         Rectangle()
+            .frame(height: 2)
+            .foregroundStyle(.lightBackground)
+            .padding(.vertical)
+    }
+}
+
 struct MissionView: View {
     struct CrewMember {
         let role: String
@@ -27,10 +36,7 @@ struct MissionView: View {
                     }
                 
                 VStack(alignment: .leading) {
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundStyle(.lightBackground)
-                        .padding(.vertical)
+                    Separator()
                     
                     Text("Mission Highlights")
                         .font(.title.bold())
@@ -38,10 +44,7 @@ struct MissionView: View {
                     
                     Text(mission.description)
                     
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundStyle(.lightBackground)
-                        .padding(.vertical)
+                    Separator()
                     
                     Text("Crew")
                         .font(.title.bold())
