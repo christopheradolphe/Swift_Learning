ca1d75aa6572d46ca80b3317655d1db9760b082c
christopheradolphe <ckwadolphe@gmail.com>
2024-06-01 19:27:59 -0400

    Toolbar button location choice and hide navigation bar

---
+++ ToolbarButtonLocationSelection/ToolbarButtonLocationSelection/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ToolbarButtonLocationSelection
+//
+//  Created by Christopher Adolphe on 2024-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello, world")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Tap") {
+                            //action code
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToolbarButtonLocationSelection/ToolbarButtonLocationSelection/ToolbarButtonLocationSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarButtonLocationSelectionApp.swift
+//  ToolbarButtonLocationSelection
+//
+//  Created by Christopher Adolphe on 2024-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarButtonLocationSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
