2889355b4f55e4f9831c2d1d2e995b79bd1f37e3
christopheradolphe <ckwadolphe@gmail.com>
2024-06-08 23:54:20 -0400

    URLSession notes

---
+++ URLSession/URLSession/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  URLSession
+//
+//  Created by Christopher Adolphe on 2024-06-08.
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+    
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async { //Function that allows app to keep running while it is running
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            //URL session returns tuple of data and metadata (metadata describes how request went)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSession/URLSession/URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionApp.swift
+//  URLSession
+//
+//  Created by Christopher Adolphe on 2024-06-08.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
