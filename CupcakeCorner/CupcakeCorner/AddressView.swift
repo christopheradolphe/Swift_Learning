7f1903733b8b5d393558af8bd50ec6dd59ae439c
christopheradolphe <ckwadolphe@gmail.com>
2024-06-09 22:53:36 -0400

    Develop cupcake corner project

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Christopher Adolphe on 2024-06-09.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
